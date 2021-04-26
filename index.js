6e68d071911195d8fc7707dc8060937b09ed1596
Aziza  iliasova <miacorporation25@gmail.com>
2021-04-26 18:32:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+let writeCards=["Ada", "Brendan", "Ali"], "birthday"];
+function writeGifts(gifts){
+for (i=0; i<gifts.length; i++) {
+  console.log('Thank you, ${gifts[i]}, for the wonderful birthday gift!');
+}
+  return gifts;
+}
+
+wrapGifts(gifts);
